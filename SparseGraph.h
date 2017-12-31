56c6db9b41acf733dad991702d15f88fc48472a2
liuyangjun <1172275496@qq.com>
2017-12-31 10:37:28 +0800

    添加了稀疏图的邻接表表示方法

--- SparseGraph.h
+++ SparseGraph.h
@@ -5,60 +5,53 @@
 #ifndef GRAPHBASICS_SPARSEGRAPH_H
 #define GRAPHBASICS_SPARSEGRAPH_H
 
-#include <cassert>
 #include <iostream>
+#include <cassert>
 #include <vector>
-using namespace std;
-class DenseGraph{//构建稠密图类，使用邻接矩阵法表示
-private:
-    int n,m;//n为图的顶点数量，m为边的数量
-    bool directed;//是否为有向图
-    vector<vector<bool>> g;//构建二维数组g作为邻接表的结构基础，其存储类型为布尔类型
-public:
-    DenseGraph(int n,bool directed ){//构造函数
-        this->n=n;
-        this->directed=directed;
-        this->m=0;
-        for(int i=0;i<n;i++){
-            g.push_back(vector<bool>(n, false));//初始化邻接表的关系全部为false，即各节点之间都不连接
-        }
-         }
 
-    ~DenseGraph(){
+using namespace std;
+class SparseGraph{//构建稀疏图，并使用邻接表结构
+private:
+    int n,m;//n为图的节点数量,m为图的边数量
+    bool directed;//表示图是否为有向图
+    vector<vector<int>> g;//使用二维数组来表示邻接表,且数据类型为int类型
+public:
+    SparseGraph(int n, bool directed){//稀疏图的构造函数
+        this->n=n;
+        this->m=0;
+        this->directed=directed;
+        for(int i=0;i<n;i++){
+            g.push_back(vector<int>());
+        }
+    }
+    ~SparseGraph(){
 
     }
 
-    int V(){//返回图中的顶点数量
+    int V(){//返回图中节点的数量
         return n;
     }
-    int E{//返回图中的边数量
-            return m;
-    };
 
-    void addEdge(int v,int w){//在v节点和w节点之间建立连接关系
+    int E(){//返回图中边的数量
+        return m;
+    }
+    //在v与w节点之间加上一条边
+    void addEdge(int v,int w){
         assert(v>=0&&v<n);
-        assert(w>=0&&w<n);//防止数组越界访问
-        if(hasEdge(v,w)) return;//v节点存在到w节点的边，则不需要进行加边操作，直接返回即可
-        g[v][w]= true;
-        m++;//增加边的数量
-        if(directed){//如果为有向图
-           return;
-        }
-        else{//如果为无向图,则邻接图成对称关系，w节点到v节点之间也一定是存在连接的
-            g[w][v]= true;
-        }
+        assert(w>=0&&w<n);//防止越界
+        g[v].push_back(w);//在v的邻接表中加入w节点
+        if(v!=w&&!directed) g[w].push_back(v);//v与w不是同一个节点且图不是有向图，才需要执行改该步骤
+        m++;
     }
 
-    bool hasEdge(int v,int w){//判断v节点到w节点之间是否已经存在边
-        assert(v>=0&&v<n);//防止越界
+    bool hasEdege(int v,int w) {//判断v节点与w节点之间是否存在边
+        assert(v>=0&&v<n);
         assert(w>=0&&w<n);
-        return g[v][w];
+        for(int i=0;i<g[v].size();i++){//遍历与v节点相连的节点
+            if(g[v][i]==w) return true;//如果在邻接串中发现其中某一个节点与w节点相同，则表明v节点与w节点之间已经存在边
+        }
+        return false;
     }
-
-
-
-
-
 
 
 };
